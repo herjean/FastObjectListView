9d54c7fbe87b16a8300cf963a887f4c84768bbf1
herjean <wong0786@e.ntu.edu.sg>
2016-08-16 23:15:25 +0800

    Added ListView and StopWatches for Comparison Purposes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,9 +29,14 @@
         private void InitializeComponent()
         {
             this.fastObjectListView1 = new BrightIdeasSoftware.FastObjectListView();
+            this.NameColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.uploadBtn = new System.Windows.Forms.Button();
-            this.NameColumn = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.fastObjectListView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,40 +45,83 @@
             this.fastObjectListView1.AllColumns.Add(this.NameColumn);
             this.fastObjectListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.NameColumn});
-            this.fastObjectListView1.Location = new System.Drawing.Point(22, 27);
+            this.fastObjectListView1.Location = new System.Drawing.Point(12, 28);
             this.fastObjectListView1.Name = "fastObjectListView1";
             this.fastObjectListView1.ShowGroups = false;
-            this.fastObjectListView1.Size = new System.Drawing.Size(782, 243);
+            this.fastObjectListView1.Size = new System.Drawing.Size(152, 243);
             this.fastObjectListView1.TabIndex = 0;
             this.fastObjectListView1.UseCompatibleStateImageBehavior = false;
             this.fastObjectListView1.View = System.Windows.Forms.View.Details;
             this.fastObjectListView1.VirtualMode = true;
             // 
-            // uploadBtn
-            // 
-            this.uploadBtn.Location = new System.Drawing.Point(22, 276);
-            this.uploadBtn.Name = "uploadBtn";
-            this.uploadBtn.Size = new System.Drawing.Size(782, 23);
-            this.uploadBtn.TabIndex = 1;
-            this.uploadBtn.Text = "Upload";
-            this.uploadBtn.UseVisualStyleBackColor = true;
-            this.uploadBtn.Click += new System.EventHandler(this.uploadBtn_Click);
-            // 
             // NameColumn
             // 
             this.NameColumn.AspectName = "Name";
+            // 
+            // uploadBtn
+            // 
+            this.uploadBtn.Location = new System.Drawing.Point(12, 277);
+            this.uploadBtn.Name = "uploadBtn";
+            this.uploadBtn.Size = new System.Drawing.Size(152, 23);
+            this.uploadBtn.TabIndex = 1;
+            this.uploadBtn.Text = "FastObjectListView";
+            this.uploadBtn.UseVisualStyleBackColor = true;
+            this.uploadBtn.Click += new System.EventHandler(this.uploadBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "ListView";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID});
+            this.listView1.Location = new System.Drawing.Point(182, 28);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(80, 243);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(816, 367);
+            this.ClientSize = new System.Drawing.Size(291, 367);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.uploadBtn);
             this.Controls.Add(this.fastObjectListView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.fastObjectListView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +131,11 @@
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button uploadBtn;
         private BrightIdeasSoftware.OLVColumn NameColumn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -23,8 +24,25 @@ namespace ListViewProject
             DialogResult result = folderBrowserDialog1.ShowDialog();
             if(result == DialogResult.OK)
             {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
                 this.fastObjectListView1.SetObjects(testClass.GET());
+                string elapsedTime = sw.Elapsed.ToString();
+                label1.Text = elapsedTime;
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            for (int i = 1; i < 10001; i++)
+            {
+                string item = "HELLO" + i.ToString();
+                listView1.Items.Add(item);
+            }
+            string elapsedTime2 = sw2.Elapsed.ToString();
+            label2.Text = elapsedTime2;
         }
     }
 }
